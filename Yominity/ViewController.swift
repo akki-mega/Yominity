6d75920ec1f210058a63363a9eb2d88cb74c86a1
A.F <akki_mawile23@yahoo.co.jp>
2018-04-21 11:48:03 +0900

    Initial Commit

---
+++ Yominity/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yominity
+//
+//  Created by A.F. on 2018/04/21.
+//  Copyright © 2018年 A.F. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
